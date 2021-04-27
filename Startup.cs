966d3cf6539e40a58326f17def04936a5b69534c
vidal987 <mvsouza.af@gmail.com>
2021-04-27 07:53:22 -0300

    primeiro e unico commit LOL

---
+++ Startup.cs
@@ -0,0 +1,68 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.EntityFrameworkCore;
+
+namespace RazorPagesMovie
+{
+    public class Startup
+{
+    public Startup(IConfiguration configuration, IWebHostEnvironment env)
+    {
+        Environment = env;
+        Configuration = configuration;
+    }
+
+    public IConfiguration Configuration { get; }
+    public IWebHostEnvironment Environment { get; }
+
+    public void ConfigureServices(IServiceCollection services)
+    {
+        if (Environment.IsDevelopment())
+        {
+            services.AddDbContext<RazorPagesMovieContext>(options =>
+            options.UseSqlite(
+                Configuration.GetConnectionString("RazorPagesMovieContext")));
+        }
+        else
+        {
+            services.AddDbContext<RazorPagesMovieContext>(options =>
+            options.UseSqlServer(
+                Configuration.GetConnectionString("MovieContext")));
+        }
+
+        services.AddRazorPages();
+    }
+
+    
+        public void Configure(IApplicationBuilder app)
+    {
+        if (Environment.IsDevelopment())
+        {
+            app.UseDeveloperExceptionPage();
+        }
+        else
+        {
+            app.UseExceptionHandler("/Error");
+            app.UseHsts();
+        }
+
+        app.UseHttpsRedirection();
+        app.UseStaticFiles();
+
+        app.UseRouting();
+
+        app.UseEndpoints(endpoints =>
+        {
+            endpoints.MapRazorPages();
+        });
+    }
+}
+}
